8c12c69a44e881564861632af65b354c44e47a6e
Erik Behrends <behrends@users.noreply.github.com>
2017-09-21 15:47:04 +0200

    Kapitel 4: Eingabe mit TextInput wird auf der Konsole ausgeben

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Keine Einträge im Tagebuch</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Tagebucheintrag erstellen"
+          returnKeyType="done"
+          onSubmitEditing={event => console.log(event.nativeEvent.text)}
+        />
       </View>
     );
   }
@@ -15,5 +21,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center'
+  },
+  input: {
+    height: 40
   }
 });
