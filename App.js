0a6d3fb75402822a846281c92b2636768513d65e
Erik Behrends <behrends@users.noreply.github.com>
2017-09-21 17:59:19 +0200

    Kapitel 4: Einträge in einer FlatList

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
-  state = { item: null };
+  state = { items: [] };
+
+  _addItem(text) {
+    this.setState({
+      items: [...this.state.items, { text, date: Date.now() }]
+    });
+  }
 
   render() {
+    let content = <Text>Keine Einträge im Tagebuch</Text>;
+    if (this.state.items.length > 0) {
+      content = (
+        <FlatList
+          style={styles.list}
+          data={this.state.items}
+          renderItem={({ item }) => <Text>{item.text}</Text>}
+          keyExtractor={item => item.date}
+        />
+      );
+    }
     return (
       <View style={styles.container}>
-        <Text>{this.state.item || 'Keine Einträge im Tagebuch'}</Text>
+        {content}
         <TextInput
           style={styles.input}
           placeholder="Tagebucheintrag erstellen"
           returnKeyType="done"
-          onSubmitEditing={event =>
-            this.setState({ item: event.nativeEvent.text })}
+          onSubmitEditing={event => this._addItem(event.nativeEvent.text)}
         />
       </View>
     );
@@ -24,6 +40,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center'
+  },
+  list: {
+    marginTop: 24
   },
   input: {
     height: 40
