d991b845a5e9f06722ecedeaa1e6916a73571003
Erik Behrends <behrends@users.noreply.github.com>
2017-09-25 15:33:23 +0200

    Kapitel 6: Uhrzeit der Erstellung anzeigen

--- js/components/JournalItemRow.js
+++ js/components/JournalItemRow.js
@@ -14,13 +14,27 @@ const TouchableItem =
 export default class JournalItemRow extends Component {
   render() {
     const { item } = this.props;
+    const date = new Date(item.date);
+    const minutes =
+      (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+    const time = `${date.getHours()}:${minutes}`;
 
     return (
       <TouchableItem>
         <View>
-          <Text>{item.text}</Text>
+          <Text numberOfLines={3}>{item.text}</Text>
+          <Text style={styles.time}>{time}</Text>
         </View>
       </TouchableItem>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  time: {
+    color: 'gray',
+    fontSize: 11,
+    fontWeight: '100',
+    alignSelf: 'flex-end'
+  }
+});
