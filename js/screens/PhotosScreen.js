0d6d2c40c55667b961a53d64b429ef51eb0fa189
Erik Behrends <behrends@users.noreply.github.com>
2017-10-10 13:17:24 +0200

    Kapitel 8: Fotogalerie und Einstellungen

--- js/screens/PhotosScreen.js
+++ js/screens/PhotosScreen.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
 export default class PhotosScreen extends Component {
+  _getPhotos(items) {
+    return items.map(item => (
+      <Image
+        style={styles.photo}
+        source={{ uri: item.photo }}
+        resizeMode="cover"
+        key={item.date}
+      />
+    ));
+  }
+
   render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center' }}>
-        <Text style={{ textAlign: 'center' }}>Fotos</Text>
-      </View>
+    const items = this.props.screenProps.items.filter(
+      item => item.photo !== null
     );
+
+    if (items.length === 0)
+      return (
+        <View style={styles.noItems}>
+          <Text style={styles.infoText}>Keine Fotos im Tagebuch.</Text>
+        </View>
+      );
+
+    return <ScrollView>{this._getPhotos(items)}</ScrollView>;
   }
 }
+
+const width = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+  noItems: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  infoText: {
+    color: 'darkslategray',
+    fontSize: 22,
+    fontWeight: '300'
+  },
+  photo: {
+    width: width,
+    height: width,
+    marginBottom: 2
+  }
+});
--- js/screens/SettingsScreen.js
+++ js/screens/SettingsScreen.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, View } from 'react-native';
+
+import Store from '../Store';
 
 export default class SettingsScreen extends Component {
+  _deleteItems() {
+    Alert.alert(
+      'Einträge löschen',
+      'Sollen wirklich alle Einträge gelöscht werden?',
+      [
+        {
+          text: 'Nein',
+          style: 'cancel'
+        },
+        {
+          text: 'Ja',
+          onPress: async () => {
+            await Store.deleteItems();
+            this.props.screenProps.refresh();
+          }
+        }
+      ]
+    );
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center' }}>
-        <Text style={{ textAlign: 'center' }}>Settings</Text>
+      <View style={styles.container}>
+        <Button
+          title="Alle Einträge löschen"
+          onPress={() => this._deleteItems()}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
