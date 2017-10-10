cc39eafa086e9321554d08eb204c0d73f8d325c5
Erik Behrends <behrends@users.noreply.github.com>
2017-10-10 12:33:28 +0200

    Kapitel 8: Tableiste unten darstellen

--- js/AppNavigator.js
+++ js/AppNavigator.js
@@ -7,34 +7,42 @@ import JournalScreen from './screens/JournalScreen';
 import PhotosScreen from './screens/PhotosScreen';
 import SettingsScreen from './screens/SettingsScreen';
 
-const Tabs = TabNavigator({
-  Journal: {
-    screen: JournalScreen,
-    navigationOptions: {
-      title: 'Tagebuch',
-      tabBarIcon: ({ tintColor }) => (
-        <SimpleLineIcons name="book-open" size={24} color={tintColor} />
-      )
+const Tabs = TabNavigator(
+  {
+    Journal: {
+      screen: JournalScreen,
+      navigationOptions: {
+        title: 'Tagebuch',
+        tabBarIcon: ({ tintColor }) => (
+          <SimpleLineIcons name="book-open" size={24} color={tintColor} />
+        )
+      }
+    },
+    Photos: {
+      screen: PhotosScreen,
+      navigationOptions: {
+        title: 'Fotos',
+        tabBarIcon: ({ tintColor }) => (
+          <SimpleLineIcons name="picture" size={24} color={tintColor} />
+        )
+      }
+    },
+    Settings: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        title: 'Einstellungen',
+        tabBarIcon: ({ tintColor }) => (
+          <SimpleLineIcons name="settings" size={24} color={tintColor} />
+        )
+      }
     }
   },
-  Photos: {
-    screen: PhotosScreen,
-    navigationOptions: {
-      title: 'Fotos',
-      tabBarIcon: ({ tintColor }) => (
-        <SimpleLineIcons name="picture" size={24} color={tintColor} />
-      )
-    }
-  },
-  Settings: {
-    screen: SettingsScreen,
-    navigationOptions: {
-      title: 'Einstellungen',
-      tabBarIcon: ({ tintColor }) => (
-        <SimpleLineIcons name="settings" size={24} color={tintColor} />
-      )
+  {
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      activeTintColor: 'deepskyblue'
     }
   }
-});
+);
 
 export default Tabs;
